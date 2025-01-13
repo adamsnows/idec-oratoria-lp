308bac2f7593b143bf55b2be913e2b0423f3e84b
Adam Neves <adaamangelow@gmail.com>
2025-01-13 14:11:22 -0300

    feat: better adjustments

--- src/components/Template/Hero/Hero.tsx
+++ src/components/Template/Hero/Hero.tsx
@@ -146,12 +146,12 @@ const Hero: React.FC = () => {
           <img
             src="/banners/formacao/oratoria.png"
             alt=""
-            className="rounded-lg max-h-[500px] max-w-[550px] mx-auto"
+            className="rounded-lg max-h-[500px] lg:max-w-[550px] mx-auto"
           />
           <img
             src="/banners/formacao/oratoria-2.png"
             alt=""
-            className="rounded-lg max-h-[500px] max-w-[550px] mx-auto"
+            className="rounded-lg max-h-[500px] lg:max-w-[550px] mx-auto"
           />
         </div>
       </div>
